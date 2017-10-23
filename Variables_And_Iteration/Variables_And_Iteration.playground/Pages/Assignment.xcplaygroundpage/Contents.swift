0657984315bdcf09b69cfe5807271018386ebb0f
Khalid Alomar <kalomar@lcs-mba3261.lcs.on.ca>
2017-10-22 21:17:50 -0400

    got some of it done

--- Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Assignment.xcplaygroundpage/Contents.swift
+++ Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Assignment.xcplaygroundpage/Contents.swift
@@ -19,9 +19,15 @@ import PlaygroundSupport
 
 // Create canvas
 let canvas = Canvas(width: 500, height: 500)
-canvas.drawShapesWithFill = false
+canvas.defaultLineWidth=7
 canvas.drawLine(fromX: 100, fromY: 100, toX: 100, toY: 100)
 canvas.drawLine(fromX: 150, fromY: 150, toX: 150, toY: 150)
+canvas.drawRectangle(centreX: 100, centreY: 100, width: 100, height: 100)
+canvas.drawLine(fromX: 400, fromY: 100, toX: 400, toY: 100)
+canvas.drawLine(fromX: 450, fromY: 100, toX: 450, toY: 100)
+canvas.drawRectangle(centreX: 400, centreY: 100, width: 400, height: 100)
+
+canvas.drawLine(fromX: 100, fromY: 400, toX: 100, toY: 400)
 
 /*:
  ## Template code
